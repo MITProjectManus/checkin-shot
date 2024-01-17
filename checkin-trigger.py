83fc4b155358ed5e5e8cb79700de35bcedec8afe
othomasprime <othomas@othom.as>
2024-01-17 16:47:17 -0500

    Stub to get started

--- checkin-trigger.py
+++ checkin-trigger.py
@@ -1,9 +1,11 @@
 #/usr/local/bin/python3
 
 from flask import Flask, json, jsonify
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+
 def lost_world():
 	response = jsonify('Lost? This is not a public API. Contact make-admin@mit.edu.')
 	response.status_code = 200
